a2fa0744dd047c2b3c21227e9cd03b49e5c13412
David Heffernan <david@davidheffernan.com>
2016-02-19 15:07:39 +0000

    Updated Convert class to account for cell type

--- JEE/CallFailureEnterprise/src/main/java/com/coolbeanzzz/development/services/Convert.java
+++ JEE/CallFailureEnterprise/src/main/java/com/coolbeanzzz/development/services/Convert.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 import jxl.Cell;
+import jxl.CellType;
 import jxl.Sheet;
 import jxl.Workbook;
 import jxl.read.biff.BiffException;
@@ -14,26 +15,33 @@ public class Convert {
 
 	public static void main(String[] args) throws IOException {
 		Convert convert = new Convert();
-		convert.setInputFile("C:/DT340A/xls/base_data.xls");
+		convert.setInputFile("./base_data.xls");
 		convert.read();
 	}
 
 	private String inputFile;
 
+	/**
+	 * Sets the xls file to be converted to json
+	 */
 	public void setInputFile(String inputFile) {
 		this.inputFile = inputFile;
 	}
-
+	
+	
+	/**
+	 * Creates a json file from each sheet in a provided xls file
+	 */
 	public void read() throws IOException  {
 		File inputWorkbook = new File(inputFile);
-		FileWriter jsonConvert=new FileWriter("convertedFile.json");
+		FileWriter jsonConvert = new FileWriter("convertedFile.json");
 		Workbook workbook;
 		ArrayList<String> labels = new ArrayList<String>();
 		
 		try {
 			workbook = Workbook.getWorkbook(inputWorkbook);
 			for(int sheetNumber = 0; sheetNumber < workbook.getNumberOfSheets(); sheetNumber++){
-				jsonConvert=new FileWriter("convertedFile" + sheetNumber + ".json");
+				jsonConvert = new FileWriter("convertedFile" + sheetNumber + ".json");
 				Sheet sheet = workbook.getSheet(sheetNumber);
 				jsonConvert.append("[");
 				for (int row = 0; row < sheet.getRows(); row++){
@@ -41,18 +49,26 @@ public class Convert {
 						jsonConvert.append("{");
 					}
 					for (int column = 0; column < sheet.getColumns(); column++) {
-						if(row==0){
+						if(row == 0){
 							Cell cell = sheet.getCell(column, row);
 							labels.add(cell.getContents());
 						}
 						else{
 							Cell cell = sheet.getCell(column, row);
+							CellType type = cell.getType();
+							String cellType = type.toString();
+							
 							jsonConvert.append("\"" + labels.get(column) + "\":");
-							if(column != sheet.getColumns() - 1){
-								jsonConvert.append("\"" + cell.getContents() + "\",");
+							
+							if(cellType.equals("Number")){
+								jsonConvert.append(cell.getContents());
 							}
 							else{
 								jsonConvert.append("\"" + cell.getContents() + "\"");
+							}
+							
+							if(column != sheet.getColumns() - 1){
+								jsonConvert.append(",");
 							}
 						}
 					}
