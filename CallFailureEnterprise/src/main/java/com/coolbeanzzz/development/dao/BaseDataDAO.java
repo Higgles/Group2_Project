a18c69e83def3a1c37d7850e8aeb68bc86bc57c7
Higgles <thomas.higgins.2@ucdconnect.ie>
2016-02-22 19:28:38 +0000

    Revert "be careful ...cont"
    This reverts commit ab6b95423dc5ef58d5c1253c897f0c1fe8dee2f6.

--- CallFailureEnterprise/src/main/java/com/coolbeanzzz/development/dao/BaseDataDAO.java
+++
@@ -1,9 +0,0 @@
-package com.coolbeanzzz.development.dao;
-
-import java.util.Collection;
-
-import com.coolbeanzzz.development.entities.BaseData;
-
-public interface BaseDataDAO {
-	Collection<BaseData> getAllBaseData();
-}
