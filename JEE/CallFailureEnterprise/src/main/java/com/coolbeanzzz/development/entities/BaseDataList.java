26f9c8c538b8b163a8d31ccb0cd84df0aecbc0bc
Thomas Higgins <higg.th@gmail.com>
2016-02-25 18:51:21 +0000

    Valid data display fix

--- JEE/CallFailureEnterprise/src/main/java/com/coolbeanzzz/development/entities/BaseDataList.java
+++ JEE/CallFailureEnterprise/src/main/java/com/coolbeanzzz/development/entities/BaseDataList.java
@@ -1,19 +1,43 @@
 package com.coolbeanzzz.development.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
 public class BaseDataList implements Serializable {
-	private Collection<BaseData> baseDataCollection;
+	private Collection<BaseDataResult> baseDataCollection;
 	
-	public Collection<BaseData> getBaseDataCollection() {
+	public Collection<BaseDataResult> getBaseDataCollection() {
         return baseDataCollection;
     }
 
     public void setBaseDataCollection(Collection<BaseData> baseDataCollection) {
-        this.baseDataCollection = baseDataCollection;
+    	Iterator<BaseData> basedata=baseDataCollection.iterator();
+    			
+        this.baseDataCollection = new ArrayList<BaseDataResult>();
+        while(basedata.hasNext()){
+        	BaseData c=basedata.next();
+	        this.baseDataCollection.add(new BaseDataResult(
+	        	c.getId(),
+	        	c.getDateTime(),
+	        	c.getEventCause().getEventId(),
+	        	c.getFailureClass().getFailureClass(),
+	        	c.getUeTable().getTac(),
+	        	c.getMccmnc().getMcc(),
+	        	c.getMccmnc().getMnc(),
+	        	c.getCellId(),
+	        	c.getDuration(),
+	        	c.getEventCause().getCauseCode(),
+	        	c.getNeVersion(),
+	        	c.getImsi(),
+	        	c.getHier3_Id(),
+	        	c.getHier32_Id(),
+	        	c.getHier321_Id()
+	        		));
+        }
     }
 }
