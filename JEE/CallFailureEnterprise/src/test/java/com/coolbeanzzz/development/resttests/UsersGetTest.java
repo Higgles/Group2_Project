a95a9749a4d3d39892ab967bd958610fc0c6b6ad
Thomas Higgins <higg.th@gmail.com>
2016-03-22 15:04:44 +0000

    Fixed minor shiro problems. Removed unused UserRestTest file.

--- JEE/CallFailureEnterprise/src/test/java/com/coolbeanzzz/development/resttests/UsersGetTest.java
+++ JEE/CallFailureEnterprise/src/test/java/com/coolbeanzzz/development/resttests/UsersGetTest.java
@@ -1,5 +1,6 @@
 package com.coolbeanzzz.development.resttests;
 
+import org.jboss.arquillian.junit.InSequence;
 import org.junit.Test;
 
 import static com.jayway.restassured.RestAssured.given;
@@ -7,6 +8,18 @@ import static com.jayway.restassured.RestAssured.given;
 public class UsersGetTest {
 
 	@Test
+	@InSequence(1)
+	public void testGetUsersWithoutLogin() {
+	given().
+		auth().basic("k", "k"). //remove login credentials
+	when().
+		get("http://localhost:8080/CallFailureEnterprise/rest/users").
+	then().
+		statusCode(401);	//unauthorized
+	}
+	
+	@Test
+	@InSequence(2)
 	public void testGetUsers() {
 	given().
 		auth().basic("admin", "admin").
@@ -15,27 +28,4 @@ public class UsersGetTest {
 	then().
 		statusCode(200);	
 	}
-
-	@Test
-	public void testGetUsersWithoutLogin() {
-	given().
-		auth().basic("", ""). //remove login credentials
-	when().
-		get("http://localhost:8080/CallFailureEnterprise/rest/users").
-	then().
-		statusCode(401);	//unauthorized
-	}
-	
-	@Test
-	public void testGetUsersAgainstPOST() {
-	given().
-		auth().basic("admin", "admin").
-	when().
-		get("http://localhost:8080/CallFailureEnterprise/rest/users/login").
-	then().
-		statusCode(405);	//Method not allowed
-	}
-	
-	
-
 }
