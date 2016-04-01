a99bd526d679422199683a7895916a0c8beb8e45
David Heffernan <david@davidheffernan.com>
2016-04-01 15:34:33 +0100

    Selenium added. Login and query tests for selenium added

---
+++ JEE/CallFailureEnterprise/src/test/java/com/coolbeanzzz/development/selenium/LoginTest.java
@@ -0,0 +1,66 @@
+package com.coolbeanzzz.development.selenium;
+
+import java.util.concurrent.TimeUnit;
+
+import junit.framework.TestCase;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class LoginTest extends TestCase {
+	
+	private WebDriver driver;
+
+	/**
+	 * Set up the web driver for selenium tests and login as Sys Admin
+	 *
+	 */
+    public void setUp() throws Exception {
+        DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+        capabilities.setCapability("version", "11");
+        capabilities.setCapability("platform", Platform.WINDOWS);
+        capabilities.setCapability("name", "Testing Selenium 2");
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    /**
+     * Test the page is loaded for login
+     * @throws Exception
+     */
+    public void testPage() throws Exception {
+        this.driver.get("http://localhost:8080/CallFailureEnterprise");
+        assertEquals("Coolbeanzzz", this.driver.getTitle());
+    }
+    
+    /**
+     * Test the login for admin user and that they have
+     * SysAd role
+     * @throws Exception
+     */
+    public void testLogin() throws Exception {
+        this.driver.get("http://localhost:8080/CallFailureEnterprise");
+        
+        WebElement username = driver.findElement(By.name("username"));
+        WebElement password = driver.findElement(By.name("password"));
+
+        username.sendKeys("admin");
+        password.sendKeys("admin");
+
+        username.submit();
+        
+        driver.get("http://localhost:8080/CallFailureEnterprise/rest/users/currentUser");
+        String user = driver.getPageSource();
+	    driver.get("http://localhost:8080/CallFailureEnterprise/upload.jsp");
+	    
+	    assert(user.contains("SysAd"));
+    }
+
+    public void tearDown() throws Exception {
+        this.driver.quit();
+    }
+}
